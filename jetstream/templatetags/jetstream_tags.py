ed5eef79a047f98ba1c325de64b4b16a9c3f1c26
Christopher Malek <cmalek@dhcp-229-221.caltech.edu>
2017-08-28 10:01:27 -0700

    BUGFIX: add missing imports and tags to jetstream_tags

--- jetstream/templatetags/jetstream_tags.py
+++ jetstream/templatetags/jetstream_tags.py
@@ -5,8 +5,17 @@ from django.utils.safestring import mark_safe
 from wagtail.wagtailembeds.exceptions import EmbedException
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailembeds import embeds
+from wagtail.wagtailimages.models import Filter
+from wagtail.wagtailimages.shortcuts import get_rendition_or_not_found
 
 register = template.Library()
+
+PHONE_WIDTH = 768
+TABLET_WIDTH = 992
+DESKTOP_WIDTH = 1200
+# We order these large to small to get sizing rules to order correctly.
+VIEWPORT_WIDTHS = [DESKTOP_WIDTH, TABLET_WIDTH, PHONE_WIDTH]
+
 
 @register.simple_tag(name='arbitrary_video')
 def arbitrary_video(video, width, height):
@@ -46,6 +55,33 @@ def arbitrary_image(parser, token):
 
     return parse_image_tag('arbitrary_image', parser, token, ArbitraryImageNode)
 
+
+@register.tag(name='responsive_image')
+def responsive_image(parser, token):
+    """
+    Usage: {% responsive_image self.image __MODE__ __WIDTH__ __HEIGHT__ [ custom-attr="value" ... ] %}
+
+    This tag works just like arbitrary_image, but generates a <picture> tag with a few responsive <source>s, instead of
+    an <img> tag. Because more than one rendition is generated, using the "as var_name" functionality would be
+    ambiguous, so {% responsive_image %} doesn't support it.
+    """
+    return parse_image_tag('responsive_image', parser, token, ResponsiveImageNode)
+
+
+class ImageNode(template.Node):
+
+    def __init__(self, image_expr, mode_expr, width_expr, height_expr, output_var_name=None, attrs=None):
+        self.image_expr = image_expr
+        self.mode_expr = mode_expr
+        self.width_expr = width_expr
+        self.height_expr = height_expr
+        self.output_var_name = output_var_name
+        self.attrs = attrs or {}
+
+    def render(self, context):
+        raise NotImplementedError
+
+
 class ResponsiveImageNode(ImageNode):
 
     def render(self, context):
@@ -80,19 +116,6 @@ class ResponsiveImageNode(ImageNode):
               {img_tag}
             </picture>
         """.format(img_tag=img_tag, full=base_rendition, square=square_rendition)
-
-class ImageNode(template.Node):
-
-    def __init__(self, image_expr, mode_expr, width_expr, height_expr, output_var_name=None, attrs=None):
-        self.image_expr = image_expr
-        self.mode_expr = mode_expr
-        self.width_expr = width_expr
-        self.height_expr = height_expr
-        self.output_var_name = output_var_name
-        self.attrs = attrs or {}
-
-    def render(self, context):
-        raise NotImplementedError
 
 
 class ArbitraryImageNode(ImageNode):
@@ -140,6 +163,7 @@ def width_from_arbitrary_parent(parent_px, units):
     """
     return int(parent_px) / 12 * int(units)
 
+
 @register.simple_tag(name='link_exists')
 def link_exists(block):
     """
@@ -148,6 +172,7 @@ def link_exists(block):
     component always exists, even if both fields are emtpy.
     """
     return block['page'] or block['url']
+
 
 @register.simple_tag()
 def page_descendants(page):
@@ -158,3 +183,73 @@ def page_descendants(page):
 def page_siblings(page):
     # Results don't include itself, so need to add a non-hyperlink dummy listing
     return page.get_siblings(inclusive=False)
+
+
+# ---------------
+# UTIL FUNCTIONS
+# ---------------
+
+
+def parse_image_tag(tag_name, parser, token, node_class):
+    """
+    Send this function the name of the tag it's parsing for, the arguments to the tag function, and the class of
+    ImageNode that you want to be constructed from the parsed data.
+    """
+    bits = token.split_contents()[1:]
+    image_expr = parser.compile_filter(bits[0])
+    mode_expr = parser.compile_filter(bits[1])
+    width_expr = parser.compile_filter(bits[2])
+    height_expr = parser.compile_filter(bits[3])
+    # The leftovers are custom-attrs and the "as var" code, if they exist.
+    leftovers = bits[4:]
+
+    attrs = {}
+    output_var_name = None
+    # If True, the next bit to be read is the output variable name.
+    as_context = False
+    # Set to False if any invalid formatting is detected.
+    is_valid = True
+
+    # Defined here because the same exception is used multiple times.
+    error = template.TemplateSyntaxError(
+        """
+        '{0}' tag should be of the form
+        {{% {0} self.photo 'max' 320 200 [ custom-attr="value" ... ] %}} or
+        {{% {0} self.photo 'max' 320 200 as img %}}.
+        You CANNOT use both custom-attr="value" and 'as img'!
+        """.format(tag_name)
+    )
+
+    for bit in leftovers:
+        if bit == 'as':
+            # Indicate that 'token' is of the form {% arbitrary_image self.photo 'max' 320 200 as img %}
+            as_context = True
+        elif as_context:
+            if output_var_name is None:
+                output_var_name = bit
+            else:
+                # More than one item exists after 'as' - reject as invalid.
+                is_valid = False
+        else:
+            try:
+                name, value = bit.split('=')
+                # Resolve context variables as value.
+                attrs[name] = parser.compile_filter(value)
+            except ValueError:
+                raise error
+
+    if as_context and output_var_name is None:
+        # Context was introduced but no variable given, which is invalid.
+        is_valid = False
+
+    if output_var_name and attrs:
+        # Extra attrs are not valid when using the 'as img' form of the tag, because we can't include them in the
+        # returned Rendition object.
+        is_valid = False
+
+    if is_valid:
+        return node_class(
+            image_expr, mode_expr, width_expr, height_expr, output_var_name=output_var_name, attrs=attrs
+        )
+    else:
+        raise error
