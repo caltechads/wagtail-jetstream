92718f4fe1b34fac42f7d4b6c8739a5421b090a2
Jeffrey Chau <jchau@caltech.edu>
2017-08-30 12:41:15 -0700

    Bump version: 0.2.6 → 0.2.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wagtail-jetstream',
-    version='0.2.6',
+    version='0.2.7',
     description='A set of complex layout blocks for use in Wagtail Streamfields',
     url='https://github.com/caltechads/wagtail-jetstream',
     author='Caltech ADS',
