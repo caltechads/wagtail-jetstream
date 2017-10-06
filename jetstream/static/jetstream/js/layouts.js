de744ba04e05eacf3d9f5f3b70902d61bac65230
Robert Rollins <rrollins@caltech.edu>
2017-10-05 17:26:40 -0700

    BUGFIX: VideoBlocks no longer require two clicks to play on mobile devices.
    My oh-so-clever solution for getting the video to play when the user clicks the
    title overlay block turned out to only worked on desktop. On mobile, I had to do
    some  shenanigans with unclickability rules to ensure that the initial tap went
    through directly to the iframe. I also added some code to make the title div
    vanish when tapped, which is needed on iPads since they don't play the video in
    full screen.
    
    The new utils.js file currently only adds a simplified Modernizr test to check
    if the user is on a mobile device.

--- jetstream/static/jetstream/js/layouts.js
+++ jetstream/static/jetstream/js/layouts.js
@@ -12,25 +12,41 @@
   }
 
   function set_up_video_blocks() {
-    $('.video-block-overlay').on('click', function () {
-      // Hide the overlay to remove the title set in the VideoBlock.
-      $(this).hide();
+    if (!Modernizr.mobile) {
+      // The .video-block-overlay div covers the entire video block. On desktop, we need to translate clicks through
+      // it and onto the iframe.
+      $('.video-block-overlay').on('click', function() {
+        // Hide the overlay so that video's controls are fully accessible.
+        $(this).hide();
 
-      var $iframe = $('iframe', $(this).parent());
-      var iframe = $iframe[0];
-      // YouTube and Vimeo videos require different actions to make this work.
-      if ($iframe.data('provider') == 'YouTube') {
-        iframe.src = iframe.src.replace('showinfo=0', 'showinfo=0&autoplay=1');
-      }
-      else if ($iframe.data('provider') == 'Vimeo') {
-        iframe.src = iframe.src.replace('portrait=0', 'portrait=0&autoplay=1');
-      }
+        var $iframe = $('iframe', $(this).parent());
+        var iframe = $iframe[0];
+        // YouTube and Vimeo videos require different actions to make this work.
+        if ($iframe.data('provider') == 'YouTube') {
+          iframe.src = iframe.src.replace('showinfo=0', 'showinfo=0&autoplay=1');
+        }
+        else if ($iframe.data('provider') == 'Vimeo') {
+          iframe.src = iframe.src.replace('portrait=0', 'portrait=0&autoplay=1');
+        }
+      });
+    }
+    else {
+      // On mobile, we set up the overlay to be untouchable; clicks will pass through it and hit the iframe directly.
+      // This doesn't hide the title, but that's OK on phones because the video plays in full screen.
+      $('.video-block-overlay').addClass('mc-hammer');
+    }
+
+    // Rergardless of whether we're on mobile, make clicks/taps on the title itself hide the overlay.
+    // This lets tablet users hide the title with a second click.
+    $('.video-block-title-background').on('click', function() {
+      $(this).parent().hide();
     });
   }
 
   $(document).ready(function() {
     set_up_video_blocks();
   });
+
   $(window).load(function() {
     make_image_cards_same_height_in_same_row();
   });
