2f1a90c87e0ce676ad913563e09f4003f4318c6c
Christopher Malek <>
2018-10-11 12:38:13 -0700

    Bump version: 1.0.9 → 1.0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wagtail-jetstream',
-    version='1.0.9',
+    version='1.0.10',
     description='A set of complex layout blocks for use in Wagtail Streamfields.',
     url='https://github.com/caltechads/wagtail-jetstream',
     author='Caltech ADS',
