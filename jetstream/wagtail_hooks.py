2438370d8f1918d8ecc9444d7a15327f2c423801
Robert Rollins <rrollins@caltech.edu>
2022-04-14 16:17:44 -0700

    BUGFIX: admin.scss now properly uses sass_processor
    Back when I was converting all of Multitenant's scss to auto-compile using django-sass-processor, I mistakenly assumed that I'd managed to get jetstream set up for all that. I had missed one little piece, though, likely because of a corruption in my PyCharm workspace that I just noticed a few minutes ago.
    
    Turns out that I never properly converted jetstream's _admin_ css to use the new system. This commit fixes that.

--- jetstream/wagtail_hooks.py
+++ jetstream/wagtail_hooks.py
@@ -1,9 +1,10 @@
 from django.utils.html import format_html
 from django.templatetags.static import static
+from sass_processor.processor import sass_processor
 
 from wagtail.core import hooks
 
 
 @hooks.register('insert_global_admin_css')
 def global_admin_css():
-    return format_html('<link rel="stylesheet" href="{}">', static('jetstream/css/admin.css'))
+    return format_html('<link rel="stylesheet" href="{}">', sass_processor('jetstream/css/admin.scss'))
