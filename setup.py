d913d323de929acb66dfdc7e39a5a084d54d45c8
Robert Rollins <rrollins@caltech.edu>
2024-11-18 14:16:54 -0800

    Bump version: 2.2.2 → 2.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wagtail-jetstream',
-    version='2.2.2',
+    version='2.3.0',
     description='A set of complex layout blocks for use in Wagtail Streamfields.',
     url='https://github.com/caltechads/wagtail-jetstream',
     author='Caltech ADS',
